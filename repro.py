63edb9f178f589ca0ca3a47cee56217e9246bb00
Graham Markall <gmarkall@nvidia.com>
2021-12-31 17:14:04 +0000

    Add reference output and check against it

--- repro.py
+++ repro.py
@@ -1,15 +1,20 @@
 """
 binary dilation in numba cuda
 >> python cuda_binary_dilate_3d.py 512 512 512 1
->>> runtime: 0.001s on a Nvidia TitanXP. 
+>>> runtime: 0.001s on a Nvidia TitanXP.
 """
 import numba
 import numpy as np
 import time
 from numba import cuda
+import pickle
+
+# Whether to check output - reference version output's pickle not stored in git
+# repo due to size.
+CHECK_OUTPUT = False
 
 
-@cuda.jit("void(uint8[:,:,:],uint8[:,:,:],uint8[:,:,:])")
+@cuda.jit
 def cuda_binary_dilate_u8(vol, out, kern):
     z, y, x = cuda.grid(3)
     d, h, w = vol.shape
@@ -31,7 +36,8 @@ def cuda_binary_dilate_u8(vol, out, kern):
                     zz = z + i - pa
                     yy = y + j - pb
                     xx = x + k - pc
-                    if zz < 0 or zz >= d or yy < 0 or yy >= h or xx < 0 or xx >= w:
+                    if (zz < 0 or zz >= d or yy < 0 or yy >= h or xx < 0
+                            or xx >= w):
                         continue
                     if vol[zz, yy, xx] and shared[i, j, k]:
                         out[z, y, x] = True
@@ -54,19 +60,32 @@ def binary_dilate_u8_cuda(vol, kern, iterations=1):
 
 
 def test(d=256, h=256, w=256, niter=1):
+    np.random.seed(1)
     vol = np.random.randn(d, h, w) > 1
     kern = np.random.randn(3, 3, 3) > 1
 
     vol = cuda.to_device(vol)
     kern = cuda.to_device(kern)
 
+    # Warm up / compile
+    res = binary_dilate_u8_cuda(vol, kern, niter)
+
+    if CHECK_OUTPUT:
+        with open('reference.pickle', 'rb') as f:
+            ref = pickle.load(f)
+
+        np.testing.assert_equal(res.copy_to_host(), ref)
+
     t1 = time.time()
     for i in range(10):
         binary_dilate_u8_cuda(vol, kern, niter)
+    cuda.synchronize()
     t2 = time.time()
 
     print(f" runtime: {(t2-t1)/10}")  # i measure 1ms
 
 
 if __name__ == "__main__":
-    test()
+    import sys
+    args = [int(a) for a in sys.argv[1:]]
+    test(*args)
